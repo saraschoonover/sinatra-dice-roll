c91b3987724183e5af777bd35bf64fac56333fae
Sara Schoonover <Saramschoon@gmail.com>
2023-07-31 19:39:57 +0000

    added loops

--- dice.rb
+++ dice.rb
@@ -30,32 +30,30 @@ end
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
-  sum = first_die + second_die
+  sum = []
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = sum.push(first_die, second_die)
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
 	second_die = rand(1..10)
-  sum = first_die + second_die
+  sum = []
 	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = sum.push(first_die, second_die)
 	
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
+	erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
+  sum = []
 	
-	outcome = "You rolled a #{die}."
+	@outcome = sum.push(die)
 	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -63,10 +61,21 @@ get("/dice/5/4") do
 	second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  sum = []
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = sum.push(first_die, second_die, third_die, fourth_die)
 	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+	erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
